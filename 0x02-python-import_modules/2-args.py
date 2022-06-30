96390765e7b7f3003a3241635b1d595be55c90d1
antishsh <antenehshimelis2019@gmail.com>
2022-06-30 10:17:11 -0700

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         if total == 2:
             print ("{:d} argument:".format(total - 1))
         else:
-            print ("{:d} argument:".format(total - 1))
+            print ("{:d} arguments:".format(total - 1))
         for i in range(1, total):
             print ("{:d}: {}".format(i, sys.argv[i]))
