b0917b1e29d24a96c098d13763a33f11db58cfb2
antishsh <antenehshimelis2019@gmail.com>
2022-07-19 11:11:40 -0700

    correction

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+""" creates class Square """
+
+
 class Square:
-     def __init__(self, size=0):
-         self.size = size
+    """ Square class"""
+    def __init__(self, size=0):
+        self.size = size
 
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, size):
         if type(size) != int:
@@ -13,5 +18,6 @@ class Square:
         elif size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     def area(self):
         return (self.__size ** 2)
