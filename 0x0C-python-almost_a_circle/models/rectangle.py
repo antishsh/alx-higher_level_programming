9d9ac4d7157a80fd2668574d3851df7e3600d335
Anteneh Shimelis <48884352+antishsh@users.noreply.github.com>
2022-08-11 10:54:46 +0300

    correction

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -119,5 +119,5 @@ class Rectangle(Base):
         """
 
         return {
-            'x': self.x, 'y': self.y, 'id': self.id, 
+            'x': self.x, 'y': self.y, 'id': self.id,
             'height': self.height, 'width': self.width}
