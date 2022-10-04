92490a7a500bda73edba5b25aa7efd3d43ebaf81
JIBRIL ADEM KIMO <JIBRILAD@ethiopianairlines.com>
2022-10-04 09:36:14 +0300

    coorr

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require("./100-data").list;
+const list = require('./100-data').list;
 const newList = list.map(function (num, index) {
   return num * index;
 });
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
 const fArg = fs.readFileSync(process.argv[2]).toString();
 const sArg = fs.readFileSync(process.argv[3]).toString();
