582d6e8a58b35713156575ff1ca39d4c4521b166
Antenehshi <AntenehShi@ethiopianairlines.com>
2022-11-15 09:11:10 -0800

    corr

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const todos = JSON.parse(body);
