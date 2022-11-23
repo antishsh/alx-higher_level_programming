8ba513a90c5d82d7ec5263cf8b46e59392463a2d
antish <AntenehShi@ethiopianairlines.com>
2022-11-23 21:21:39 +0300

    change

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,3 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- grants SELECT privileges to a user
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+
+CREATE USER IF NOT EXISTS replica_user@%;
+-- grants SELECT privileges to a user
+GRANT ALL PRIVILEGES ON user .* TO replica_user@%;
